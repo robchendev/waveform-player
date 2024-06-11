2cf74d1cb495a28df76cfc187e19d4bf7014e50e
robchendev <robchendev@gmail.com>
2024-06-11 15:27:03 -0700

    Initial commit, boilerplate nextjs app

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  // 1. Get audio file 
+  // 2. confirm it is an audio file
+  // 3. show audio waveform
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-800">
+
+    </main>
+  );
+}
